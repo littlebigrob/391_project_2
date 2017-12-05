324518d8f24a05c07bf9fc5332b6a5e114d8003b
littlebigrob <robertcolin88@yahoo.com>
2017-12-04 19:37:46 -0500

    need to fix axis

--- src/project_2.py
+++ src/project_2.py
@@ -4,16 +4,16 @@ import csv
 import numpy as np
 
 
-def get_data():
-    with open('irisdata.csv') as csvfile:
+def get_data(file):
+    with open(file) as csvfile:
         reader = csv.reader(csvfile)
         data_versicolor = []
         data_virginica = []
         for row in reader:
             if row[4] == 'versicolor':
-                data_versicolor.append(row[2:3])
+                data_versicolor.append((row[2], row[3]))
             elif row[4] == 'virginica':
-                data_virginica.append(row[2:3])
+                data_virginica.append((row[2], row[3]))
     return data_versicolor, data_virginica
 
 
@@ -21,8 +21,8 @@ def split_data(data):
     petal_length = list()
     petal_width = list()
     for row in data:
-        petal_length.append(row[1])
-        petal_width.append(row[2])
+        petal_length.append(row[0])
+        petal_width.append(row[1])
     return petal_length, petal_width
 
 
@@ -33,15 +33,20 @@ def plot_line(m, b):
 
 
 def plot_for_part_one():
-    mat_plt.figure(1)
     mat_plt.title("petal length v width for versicolor and virginica")
-    data_versicolor, data_virginica = get_data()
+    data_versicolor, data_virginica = get_data('..\data\irisdata.csv')
     x1, y1 = split_data(data_versicolor)
     x2, y2 = split_data(data_virginica)
-    mat_plt.plot(x1, y1)
-    mat_plt.plot(x2, y2, 'y')
     #plot_line(1, 2)
+    mat_plt.scatter(x2, y2)
+    mat_plt.scatter(x1, y1)
     mat_plt.show()
 
-
-plot_for_part_one()
+mat_plt.title("petal length v width for versicolor and virginica")
+data_versicolor, data_virginica = get_data('..\data\irisdata.csv')
+x1, y1 = split_data(data_versicolor)
+x2, y2 = split_data(data_virginica)
+#plot_line(1, 2)
+mat_plt.scatter(x2, y2)
+mat_plt.scatter(x1, y1)
+mat_plt.show()
